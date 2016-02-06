6c2a25e46b5fccac09d3084210d134ce73dea21a
Tristan Morgan <tristannmorgann@gmail.com>
2016-02-06 17:58:42 +1100

    fedBy now a user not a string

--- cloud/main.js
+++ cloud/main.js
@@ -2,13 +2,16 @@
 Parse.Cloud.define('feedPet', function(req, res) {
 	Parse.Cloud.useMasterKey();
 	console.log('[feedPet] Info=\'Running cloud code\' petId=' + req.params.petId + ' fedBy=' + req.params.fedBy + ' fedAt=' + req.params.fedAt);
-	var FeedingLog = Parse.Object.extend("FeedingLog");
+	var FeedingLog = Parse.Object.extend('FeedingLog');
 	var feedingLog = new FeedingLog();
 	
-	feedingLog.set('fedBy', req.params.fedBy);
+	var User = Parse.Object.extend('_User');
+	var user = new User();
+	user.id = req.params.fedBy;
+	feedingLog.set('fedBy', user);
 	feedingLog.set('fedAt', req.params.fedAt); 
 	
-	var Pet = Parse.Object.extend("Pet");
+	var Pet = Parse.Object.extend('Pet');
 	var queryPet = new Parse.Query(Pet);
 	
 	var mPet;

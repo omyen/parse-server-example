e11a75d11bc82bc5fe72511b99da265143688d22
Tristan Morgan <tristannmorgann@gmail.com>
2016-03-20 13:26:55 +1100

    removed livequery line

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
